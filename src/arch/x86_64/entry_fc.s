19c204dc7789b54257cd1005610097167ba0730e
moehr1z <mooehr1z@gmail.com>
2024-11-25 10:36:32 +0100

    Use default loader for firecracker but replace multiboot handling
    UVMM can't handle starting directly with 64 bit instruction as is done
    in the firecracker loader. The default loader works but uses multiboot,
    so this was replaced with how the firecracker version does it.

--- src/arch/x86_64/entry_fc.s
+++ src/arch/x86_64/entry_fc.s
@@ -1,110 +1,16 @@
 # This is the kernel's entry point, if Hermit is running with
 # FireCracker. FireCracker assumes a 64 bit Linux kernel.
 
-.code64
+.code32
 
 .set BOOT_STACK_SIZE, 4096
 
 .extern loader_start		# defined in linker script
 .extern loader_end
 
-# Move entry point at the beginning of the elf file
 .section .mboot, "a"
 .align 8
-.global _start
-_start:
-    cli # avoid any interrupt
 
-    # Initialize stack pointer
-    movabs rsp, OFFSET boot_stack
-    add rsp, BOOT_STACK_SIZE - 16
-
-    mov [boot_params], rsi
-
-    # initialize page tables
-    # map kernel 1:1
-    push rdi
-    push rbx
-    push rcx
-    movabs rcx, OFFSET loader_start
-    movabs rbx, OFFSET loader_end
-    add rbx, 0x1000
- L0: cmp rcx, rbx
-    jae L1
-    mov rax, rcx
-    and eax, 0xFFFFF000       # page align lower half
-    mov rdi, rax
-    shr rdi, 9                # (edi >> 12) * 8 (index for boot_pgt)
-    add rdi, OFFSET boot_pgt1
-    or rax, 0x3               # set present and writable bits
-    mov [rdi], rax
-    add rcx, 0x1000
-    jmp L0
- L1:
-    pop rcx
-    pop rbx
-    pop rdi
-
-    # Set CR3
-    mov eax, OFFSET boot_pml4
-    ;or eax, (1 << 0)        # set present bit
-    mov cr3, rax
-
-    # we need to enable PAE modus
-    mov rax, cr4
-    or eax, 1 << 5
-    mov cr4, rax
-
-    # switch to the compatibility mode (which is part of long mode)
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
-
-    # Set CR4
-    mov rax, cr4
-    and eax, 0x00000000fffbf9ff     # disable SSE
-    ;or eax, (1 << 7)       # enable PGE
-    mov cr4, rax
-     
-    # Set CR0 (PM-bit is already set)
-    mov rax, cr0
-    and rax, ~(1 << 2)      # disable FPU emulation
-    or eax, (1 << 1)        # enable FPU montitoring
-    and rax, ~(1 << 30)     # enable caching
-    and rax, ~(1 << 29)     # disable write through caching
-    and rax, ~(1 << 16)	    # allow kernel write access to read-only pages
-    or eax, (1 << 31)       # enable paging
-    mov cr0, rax
-
-
-    lgdt [GDT64.Pointer] # Load the 64-bit global descriptor table.
-    jmp start64 # Set the code segment and enter 64-bit long mode.
-
-.section .text
-.align 8
-start64:
-    # initialize segment registers
-    mov ax, OFFSET GDT64.Data
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    xor ax, ax
-    mov fs, ax
-    mov gs, ax
-    cld
-    # set default stack pointer
-    movabs rsp, OFFSET boot_stack
-    add rsp, BOOT_STACK_SIZE-16
-
-    # jump to the boot processors's C code
-    .extern loader_main
-    jmp loader_main
-invalid:
-    jmp invalid
-
-.section .data
 .align 4
 # we need already a valid GDT to switch in the 64bit modus
 GDT64:                      # Global Descriptor Table (64-bit).
@@ -133,6 +39,146 @@ GDT64.Pointer:              # The GDT-pointer.
     .2byte . - GDT64 - 1    # Limit.
     .8byte GDT64            # Base.
 
+.section .text
+.align 4
+.global _start
+_start:
+    cli # avoid any interrupt
+
+    # Initialize stack pointer
+    mov esp, OFFSET boot_stack
+    add esp, BOOT_STACK_SIZE - 16
+
+    mov [boot_params], esi
+
+# This will set up the x86 control registers:
+# Caching and the floating point unit are enabled
+# Bootstrap page tables are loaded and page size
+# extensions (huge pages) enabled.
+cpu_init:
+
+    # initialize page tables
+    # map kernel 1:1
+    push edi
+    push ebx
+    push ecx
+    mov ecx, OFFSET loader_start
+    mov ebx, OFFSET loader_end
+    add ebx, 0x1000
+L0: cmp ecx, ebx
+    jae L1
+    mov eax, ecx
+    and eax, 0xFFFFF000       # page align lower half
+    mov edi, eax
+    shr edi, 9                # (edi >> 12) * 8 (index for boot_pgt)
+    add edi, OFFSET boot_pgt1
+    or eax, 0x3               # set present and writable bits
+    mov [edi], eax
+    add ecx, 0x1000
+    jmp L0
+L1:
+    pop ecx
+    pop ebx
+    pop edi
+
+    # check for long mode
+
+    # do we have the instruction cpuid?
+    pushfd
+    pop eax
+    mov ecx, eax
+    xor eax, 1 << 21
+    push eax
+    popfd
+    pushfd
+    pop eax
+    push ecx
+    popfd
+    xor eax, ecx
+    jz Linvalid
+
+    # cpuid > 0x80000000?
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb Linvalid # It is less, there is no long mode.
+
+    # do we have a long mode?
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29 # Test if the LM-bit, which is bit 29, is set in the D-register.
+    jz Linvalid # They aren't, there is no long mode.
+
+    # Set CR3
+    mov eax, OFFSET boot_pml4
+    # or eax, (1 << 0)        # set present bit
+    mov cr3, eax
+
+    # we need to enable PAE modus
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    # switch to the compatibility mode (which is part of long mode)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    # Set CR4
+    mov eax, cr4
+    and eax, 0xfffbf9ff     # disable SSE
+    # or eax, (1 << 7)      # enable PGE
+    mov cr4, eax
+
+    # Set CR0 (PM-bit is already set)
+    mov eax, cr0
+    and eax, ~(1 << 2)      # disable FPU emulation
+    or eax, (1 << 1)        # enable FPU montitoring
+    and eax, ~(1 << 30)     # enable caching
+    and eax, ~(1 << 29)     # disable write through caching
+    and eax, ~(1 << 16)	    # allow kernel write access to read-only pages
+    or eax, (1 << 31)       # enable paging
+    mov cr0, eax
+
+    lgdt [GDT64.Pointer] # Load the 64-bit global descriptor table.
+    # https://github.com/llvm/llvm-project/issues/46048
+    .att_syntax prefix
+    # Set the code segment and enter 64-bit long mode.
+    ljmp $GDT64.Code, $start64
+    .intel_syntax noprefix
+
+# there is no long mode
+Linvalid:
+    jmp Linvalid
+
+.code64
+start64:
+    # initialize segment registers
+    mov ax, OFFSET GDT64.Data
+    mov ds, eax
+    mov es, eax
+    mov ss, eax
+    xor ax, ax
+    mov fs, eax
+    mov gs, eax
+    cld
+    # set default stack pointer
+    movabs rsp, OFFSET boot_stack
+    add rsp, BOOT_STACK_SIZE-16
+
+    # jump to the boot processors's C code
+    .extern loader_main
+    jmp loader_main
+    jmp start64+0x28
+
+.section .data
+
+.global mb_info
+.align 8
+mb_info:
+    .8byte 0
+
 .global boot_params
 .align 8
 boot_params:
@@ -160,3 +206,4 @@ boot_pgt1:
     .fill 512, 8, 0
 boot_pgt2:
     .fill 512, 8, 0
+
